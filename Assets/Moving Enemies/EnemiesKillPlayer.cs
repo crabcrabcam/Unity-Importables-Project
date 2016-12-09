a37685cd870c90e9410f4285eac120d762c3f088
crabcrabcam <cam12win@gmail.com>
2016-12-09 14:25:53 +0000

    Added moving enemies, still not quite sure what to call it

---
+++ Assets/Moving Enemies/EnemiesKillPlayer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// This is the file that should be attached to all the things you want to use to kill the player
+/// Right now it just kills the player instantly
+/// </summary>
+public class EnemiesKillPlayer : MonoBehaviour
+{
+    //Gets the levelmanager so we can use the respawn part from that
+    public LevelManager levelManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Gets the level manager object in the level
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// When something enters the trigger zone (Box collider) of the object that this script is attached to
+    /// Run this function
+    /// </summary>
+    /// <param name="other">Other.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Tells you in the debuglog that something entered the collider, just in case something else breaks
+        print("Something entered the collider");
+        //Sees what the name of the object that entered is
+        if (other.name == "Player")
+        {
+            //If it's player, then respawn them with the respawn player thing
+            print("Entered the death");
+            //Calls the levelmanager script to respawn the player
+            //It is done without anything special and fancy but you could add some animation if you wanted to
+            levelManager.RespawnPlayer();
+        }
+
+        //If an enemy goes onto a death box the enemy gets destroyed
+        //This checks the tag of the object rather than the name. This is because you'll have multiple enemies with different names
+        //and you'll want them all to be killed with this
+        if (other.tag == "Enemy")
+        {
+
+            //Tells you that the enemy has been killed
+            print("Enemy entered collider and was destroyed");
+            //Destroys the "other" object. This is the object that entered the collider and is the enemy. 
+            //this does it without a fancy show and dance, but you could add an animation
+            Destroy(other);
+
+        }
+
+
+    }
+}
---
+++ Assets/Moving Enemies/EnemyMovement.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// This is the code to make the enemy move
+/// They will move left and right for the amount of time you specify. 
+/// They will go through blocks so just make sure they don't. 
+/// 
+/// </summary>
+public class EnemyMovement : MonoBehaviour
+{
+
+    //Time to move for
+    public float timeToMoveFor;
+    public float speed;
+
+    //The time the player has moved for
+    private int timeMoved = 0;
+
+    //The direction of movement. True for left and false for right. Better than a string or a number
+    public bool movingLeft;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// Called as fast as possible so not very good if it's needed to be steady. 
+    /// </summary>
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Called a fixed number of times
+    /// </summary>
+    void FixedUpdate()
+    {
+        timeCounter();
+        Movement();
+    }
+
+    //50 time = 1 unit at 1 speed .:. tickrate = 50
+    /// <summary>
+    /// The timer to see how long the enemy has been moving for and to change their movement direction
+    /// </summary>
+    void timeCounter()
+    {
+        //Adds the clock
+        timeMoved += 1;
+
+        //Checks if the time moved is more than the time you want to move for. 
+        //Using greater than rather than equals removes the ability for it to break at a slight lag
+        if (timeMoved > timeToMoveFor)
+        {
+            //Changes the direction
+            movingLeft = !movingLeft;
+            //Sets the moved time to 0 again
+            timeMoved = 0;
+            //Puts the fact it's changing direction in the log
+            print("Distance is distance, changed over");
+        }
+
+    }
+
+    /// <summary>
+    /// The moving of the enemy
+    /// </summary>
+    void Movement()
+    {
+        //Checks what direction to go
+        if (movingLeft)
+        {
+            //When it's moving left
+            //Moves the enemy to the left, at the speed specified
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        }
+        else
+        {
+            //When moving right
+            //Moves the enemy to the right, at the speed specified
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        }
+
+    }
+
+
+
+}
